40582cdad45acbc2ece888eda540df1c62c17dd6
Nathan Litzinger <nlitz88@gmail.com>
2024-12-05 12:35:24 -0500

    Added combined launch file

---
+++ robot/ros_ws/src/wheelsafe_gps_bringup/launch/wheelsafe_bringup.launch.py
@@ -0,0 +1,103 @@
+import os
+import math
+
+import ament_index_python
+
+from launch import LaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch_ros.actions import Node, SetRemap
+
+# Path to the launch files and directories that we will use
+_MICROSTRAIN_LAUNCH_FILE = os.path.join(
+    ament_index_python.packages.get_package_share_directory(
+        "microstrain_inertial_driver"
+    ),
+    "launch",
+    "microstrain_launch.py",
+)
+_GQ7_PARAMS_FILE = os.path.join(
+    ament_index_python.packages.get_package_share_directory("wheelsafe_gps_bringup"),
+    "config",
+    "gq7.yaml",
+)
+_RVIZ_DISPLAY_FILE = os.path.join(
+    ament_index_python.packages.get_package_share_directory("wheelsafe_gps_bringup"),
+    "config",
+    "display.rviz",
+)
+
+print(f"Using params file at {_GQ7_PARAMS_FILE}")
+print(f"Using rviz file at {_RVIZ_DISPLAY_FILE}")
+
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            # Microstrain node
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(_MICROSTRAIN_LAUNCH_FILE),
+                launch_arguments={
+                    "configure": "true",
+                    "activate": "true",
+                    "params_file": _GQ7_PARAMS_FILE,
+                    "namespace": "/",
+                }.items(),
+            ),
+            # Publish a static transform for where the GQ7 is mounted on base_link.
+            # Unless the GQ7 is mounted exactly one meter above base_link, you should
+            # change this to be accurate to your setup
+            # NOTE: This is the transform FROM the base_link frame TO the gq7_link
+            # frame. I.e., relative orientation from base_link frame to gq7_link frame,
+            # and subtracting the position of the gq7_link frame expressed in the
+            # baes_link frame.
+            Node(
+                package="tf2_ros",
+                executable="static_transform_publisher",
+                output="screen",
+                arguments=[
+                    "--x",
+                    "-0.17",
+                    "--y",
+                    "-0.22",
+                    "--z",
+                    "0",
+                    "--roll",
+                    "3.14",  # 180
+                    "--pitch",
+                    "0",
+                    "--yaw",
+                    "1.57",  # 90
+                    "--frame-id",
+                    "base_link",
+                    "--child-frame-id",
+                    "gq7_link",
+                ],
+            ),
+            # TODO: Add launch action for bringing up the copley node here.
+            Node(
+                package="copley_ros",
+                executable="copley_node",
+                output="screen",
+                remappings=[],  # Add remappings here
+                parameters=[],  # Add parameters here
+            ),
+            # TODO: Add launch action for bringing up the joy-linux node here.
+            Node(
+                package="joy_linux",
+                executable="joy_linux_node",
+                output="screen",
+                remappings=[],  # Add remappings here
+                parameters=[],  # Add parameters here
+            ),
+            # TODO: Add launch action for bringing up the teleop-twist-joy node
+            # here.
+            Node(
+                package="teleop_twist_joy",
+                executable="teleop_node",
+                output="screen",
+                remappings=[],  # Add remappings here
+                parameters=[],  # Add parameters here
+            ),
+        ]
+    )
